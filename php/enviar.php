256909df31b7bd3dbac0d9d8bb741535dfd83ca8
Hans Garcia <hansvekoni@gmail.com>
2022-08-06 16:48:22 -0300

    gap objetos agregado

--- php/enviar.php
+++ php/enviar.php
@@ -8,4 +8,5 @@
 	if(mail('hansvekoni@gmail.com', $asunto, $mensaje)){
 		echo "Correo enviado";
 	}
+
  ?>
---
+++ php/pf.php
@@ -0,0 +1,7 @@
+<?php 
+/* import there as <div class="menu">
+<?php include 'menu.php';?>
+</div> */
+echo " helow world"<br>;
+//ECHO " helow world"<br>;
+?>
