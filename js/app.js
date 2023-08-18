3f7553a0181e5313cd71c14026cb75bb9a699cbf
Hans <hansvekoni@gmail.com>
2023-08-18 05:19:44 -0300

    update added a css carousel

---
+++ js/app.js
@@ -0,0 +1,30 @@
+'use strict'
+
+const grande = document.querySelector('.grande'); 
+const punto  = document.querySelectorAll('.punto');
+// Cuando CLICK en punto
+    // Saber la posición de ese punto
+    // Aplicar un transform translateX al grande
+    // QUITAR la clase activo de TODOS puntos
+    // AÑADIR la clase activo al punto que hemos hecho CLICK
+
+punto.forEach((item, index) => {
+    punto[index].addEventListener('click', () => {
+        let posicion = index;
+        //operacion = posicion inicial(0%) * -50;
+        let operacion = posicion * -33;//
+        grande.style.transform = `translateX(${operacion}%)`;
+
+        punto.forEach(punto => punto.classList.remove('activo'));
+        item.classList.add('activo');
+        /* 
+        // Recorremos TODOS los punto
+        punto.forEach( ( cadaPunto , i )=>{
+            // Quitamos la clase ACTIVO a TODOS los punto
+            punto[i].classList.remove('activo')
+        })
+        // Añadir la clase activo en el punto que hemos hecho CLICK
+        punto[i].classList.add('activo')
+         */
+    });
+})
