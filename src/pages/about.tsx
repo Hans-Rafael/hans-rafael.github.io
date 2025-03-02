3876969d70da1034a4230869e4bfe619bd2ec0b6
Hans <hansvekoni@gmail.com>
2025-03-02 09:35:53 -0300

    project initialized on vite

--- src/pages/about.tsx
+++
@@ -1,8 +0,0 @@
-import MainLayout from '@/layouts/MainLayout';
-export default function AboutPage() {
-    return (
-      <MainLayout title='about con traductor'>
-        <h1>Sobre mí</h1>
-      </MainLayout>
-    );
-  }
