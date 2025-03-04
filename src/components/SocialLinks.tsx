106ae8118b2d6e5addfb51d35b46fca400525394
Hans <hansvekoni@gmail.com>
2025-03-04 15:22:07 -0300

    fix: linkedin link fixed

--- src/components/SocialLinks.tsx
+++ src/components/SocialLinks.tsx
@@ -2,7 +2,7 @@ export default function SocialLinks() {
     return (
       <div className="social-links">
         <a href="https://github.com/Hans-Rafael" target="_blank">GitHub</a>
-        <a href="linkedin.com/in/hans-garcia-developer" target="_blank">LinkedIn</a>
+        <a href="https://linkedin.com/in/hans-garcia-developer" target="_blank">LinkedIn</a>
         <a href="mailto:hansgarcia@tutamail.com">Email</a>
       </div>
     );
