7a59da77c8d5145a6a40b59fe67b6c39f2852ed7
Hans <hansvekoni@gmail.com>
2025-03-04 16:38:11 -0300

    added a new link

--- src/components/SocialLinks.tsx
+++ src/components/SocialLinks.tsx
@@ -3,6 +3,7 @@ export default function SocialLinks() {
       <div className="social-links">
         <a href="https://github.com/Hans-Rafael" target="_blank">GitHub</a>
         <a href="https://linkedin.com/in/hans-garcia-developer" target="_blank">LinkedIn</a>
+        <a href="https://gist.github.com/Hans-Rafael" target="_blank">GistHub</a>
         <a href="mailto:hansgarcia@tutamail.com">Email</a>
       </div>
     );
