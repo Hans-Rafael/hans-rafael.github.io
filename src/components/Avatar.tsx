7238ff227c5f8321bb30e80b70be8bfd901a3774
Hans <hansvekoni@gmail.com>
2025-03-04 11:20:45 -0300

    Avatar and socialLink card created

---
+++ src/components/Avatar.tsx
@@ -0,0 +1,9 @@
+export default function Avatar(){
+    return(
+        <img
+        src="/images/profile.png"
+        alt="Hans Rafael"
+        className="avatar"
+        />
+    )
+}
---
+++ src/components/SocialLinks.tsx
@@ -0,0 +1,10 @@
+export default function SocialLinks() {
+    return (
+      <div className="social-links">
+        <a href="https://github.com/Hans-Rafael" target="_blank">GitHub</a>
+        <a href="linkedin.com/in/hans-garcia-developer" target="_blank">LinkedIn</a>
+        <a href="mailto:hansgarcia@tutamail.com">Email</a>
+      </div>
+    );
+  }
+  
