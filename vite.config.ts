f5af7e0ed07086a2a64c4267b6273990c19a14c1
Hans <hansvekoni@gmail.com>
2025-03-04 12:45:53 -0300

    x

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/hans-rafael.github.io',//'/', // Ajustar esta ruta según mi repositorio, URL base de GitHub Pages
+  base:'/', // Ajustar esta ruta según mi repositorio, URL base de GitHub Pages
 })
+//'/hans-rafael.github.io',
