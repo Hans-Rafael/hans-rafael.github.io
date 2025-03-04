8bb698818c89247a04cd8325b7bf499a4c429f08
Hans <hansvekoni@gmail.com>
2025-03-04 12:40:17 -0300

    Update vite.config.ts for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Ajustar esta ruta según mi repositorio, URL base de GitHub Pages
+  base: '/hans-rafael.github.io',//'/', // Ajustar esta ruta según mi repositorio, URL base de GitHub Pages
 })
